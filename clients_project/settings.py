cc5a086871fb43a83fefb9168cac9c8ee08c5ca2
jbhonest <jbhonest@live.com>
2024-05-17 02:49:10 +0330

    minor changes

--- clients_project/settings.py
+++ clients_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 # from .local_settings import *
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,4 +128,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'clients_app:login'
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
